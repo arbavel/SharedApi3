ff1fca9d19d0fef3ae293827aa88ea2844ee673c
arbavel <fabvfabv.@gmail.com>
2024-05-17 16:29:02 -0500

    Add ClientEntity desde rama client

---
+++ SharedApi3/Shared.Common/Entities/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Common.Entities
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Region { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
