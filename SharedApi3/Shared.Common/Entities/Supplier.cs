14707a3c364fc6ad0c98396cbf5e67ecb72ce1fd
arbavel <fabvfabv.@gmail.com>
2024-05-17 16:33:15 -0500

    Add SpplierEntity from supplier branch

---
+++ SharedApi3/Shared.Common/Entities/Supplier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Common.Entities
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+        public string Region { get; set; } = null!;
+        public string PostalCode { get; set; } = null!;
+        public string Country { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
