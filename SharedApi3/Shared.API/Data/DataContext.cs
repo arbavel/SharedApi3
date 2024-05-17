c61975f0bbcac93c85ba010a292b984f21dbd683
arbavel <fabvfabv.@gmail.com>
2024-05-17 11:26:52 -0500

    Initial and create UserEntity

---
+++ SharedApi3/Shared.API/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Common.Entities;
+
+namespace Shared.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> option) : base(option)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().HasIndex(x=>x.Email).IsUnique();
+        }
+    }
+}
---
+++ SharedApi3/Shared.Common/Entities/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Common.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(10)]
+        [Length(10, 10)]
+        public string Phone { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MaxLength(100)]
+        [Length(4, 20)]
+        public string Password { get; set; } = null!;
+    }
+}
