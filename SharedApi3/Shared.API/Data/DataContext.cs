8df614dd4508796f914b6f4c69b13446ab201efc
arbavel <fabvfabv.@gmail.com>
2024-05-17 19:55:01 -0500

    Areglando

--- SharedApi3/Shared.API/Data/DataContext.cs
+++ SharedApi3/Shared.API/Data/DataContext.cs
@@ -13,7 +13,7 @@ namespace Shared.API.Data
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Client> Clients { get; set; }
-
+        public DbSet<Supplier> Suppliers { get; set; }
 
 
 
@@ -26,6 +26,7 @@ namespace Shared.API.Data
             modelBuilder.Entity<Employee>().HasIndex(x => x.Email).IsUnique();
             modelBuilder.Entity<Product>().HasIndex(x => x.Name).IsUnique();
             modelBuilder.Entity<Client>().HasIndex(x => x.Email).IsUnique();
+            modelBuilder.Entity<Supplier>().HasIndex(x => x.Email).IsUnique();
 
 
 
